c7d885f1c3c48545a402d32f5e36c48ae79e440b
agad70696-coder <agad70696@gmail.com>
2026-07-29 04:20:01 +0300

    Initialize init.py with imports and exports

---
+++ IRRE/knowledge/init.py
@@ -0,0 +1,4 @@
+from.ontology import Entity, Relation, ApexOntology
+from.knowledge_graph import KnowledgeGraph
+from.semantic_engine import SemanticEngine
+__all__ = ["Entity", "Relation", "ApexOntology", "KnowledgeGraph", "SemanticEngine"]
