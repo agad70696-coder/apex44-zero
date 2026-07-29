3478963ca6cbb5ec87be9e488d31b9beb8bc6fae
agad70696-coder <agad70696@gmail.com>
2026-07-29 04:39:32 +0300

    Create init.py

---
+++ IRRE/linguistics/init.py
@@ -0,0 +1,4 @@
+from.cultural_context import EgyptianCulturalContext, SocialIntent
+from.semantic_nlp import DeepSemanticAnalyzer
+from.linguistic_logic import LinguisticLogic
+__all__ = ["EgyptianCulturalContext", "SocialIntent", "DeepSemanticAnalyzer", "LinguisticLogic"]
