0ff5fdeaf17b44df5cffc2e2aee0d51bcaa010a6
agad70696-coder <agad70696@gmail.com>
2026-07-29 04:48:13 +0300

    Initialize quantum module with imports and exports

---
+++ IRRE/quantum/init.py
@@ -0,0 +1,4 @@
+from.quantum_logic import Qubit, QuantumState, Hadamard, CNOT
+from.quantum_reasoner import QuantumReasoner
+from.quantum_ai import QuantumAI
+__all__ = ["Qubit", "QuantumState", "Hadamard", "CNOT", "QuantumReasoner", "QuantumAI"]
