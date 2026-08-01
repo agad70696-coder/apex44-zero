2594560961d291939cbb992f2dd483ce0fe65280
agad70696-coder <agad70696@gmail.com>
2026-08-01 21:13:17 +0300

    Delete IRRE/knowledge/init.py

--- IRRE/knowledge/init.py
+++
@@ -1,4 +0,0 @@
-from.ontology import Entity, Relation, ApexOntology
-from.knowledge_graph import KnowledgeGraph
-from.semantic_engine import SemanticEngine
-__all__ = ["Entity", "Relation", "ApexOntology", "KnowledgeGraph", "SemanticEngine"]
